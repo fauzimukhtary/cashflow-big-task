174cb1642ed6ff66685f9b70f47079d5aaf40540
fauzimukhtary <fzii.ramadhan5106@gmail.com>
2026-06-05 20:41:41 +0700

    [FZ] feat: adding Main menu decoration

--- main.go
+++ main.go
@@ -1,24 +1,45 @@
 package main
 
+import "fmt"
+
 const NMAX int = 999
 
-type arrNamaMahasiswa [NMAX]string
-type arrID [NMAX]int
-
-type Master struct {
-	ID      arrID
-	Nama    arrNamaMahasiswa
-	tanggal arrTanggal
-}
-
-type arrTanggal [NMAX]tanggal
-
-type tanggal struct {
+type stTanggal struct {
 	date  int
 	month int
 	year  int
 }
 
-func main() {
+type tanggal [31]stTanggal
 
+type arrNamaMahasiswa [NMAX]string
+type arrID [NMAX]int
+type arrTanggal [NMAX]tanggal
+
+type Bulanan struct {
+	ID      arrID
+	Nama    arrNamaMahasiswa
+	Tanggal arrTanggal
+}
+
+type Iuran [12]Bulanan
+
+func main() {
+	var lanjut bool
+
+	lanjut = true
+
+	for lanjut {
+		fmt.Println("+----------------------------------------------------------+")
+		fmt.Println("|   _____   ___   _____ _   _ ______ _     _____  _    _   |")
+		fmt.Println("|  /  __ \\ / _ \\ /  ___| | | ||  ___| |   |  _  || |  | |  |")
+		fmt.Println("|  | /  \\// /_\\ \\\\ `--.| |_| || |_  | |   | | | || |  | |  |")
+		fmt.Println("|  | |    |  _  | `--. \\  _  ||  _| | |   | | | || |/\\| |  |")
+		fmt.Println("|  | \\__/\\| | | |/\\__/ / | | || |   | |___\\ \\_/ /\\  /\\  /  |")
+		fmt.Println("|   \\____/\\_| |_/\\____/\\_| |_/\\_|   \\_____/\\___/  \\/  \\/   |")
+		fmt.Println("|                                                          |")
+		fmt.Println("+----------------------------------------------------------+")
+
+		lanjut = false
+	}
 }
